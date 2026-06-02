af8c90e27c0f91017721e7b724039bda0becb55c
Pedro Henrique <pedrohenriquehernandescarvalho@gmail.com>
2026-06-01 23:20:02 -0300

    fé a ultima

--- src/app.js
+++ src/app.js
@@ -25,7 +25,6 @@ app.use(session({
 }))
 
 app.set("views", path.join(__dirname, "views"))
-
 app.use(express.static(path.join(__dirname, "public")))
 app.use(flash())
 app.use((req, res, next) => {
